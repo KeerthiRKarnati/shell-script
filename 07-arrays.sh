f9261cd58f782555b32a29e2e058b193b5f8346b
Keerthi K <keerthireddyk33@gmail.com>
2023-12-16 18:53:15 +0530

    06-data-types.sh

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango" "Grapes")
+
+echo "First Value: ${FRUITS[0]}"
+
+echo "Second Value: ${FRUITS[1]}"
+
+echo "Third Value: ${FRUITS[2]}"
+
+echo "Fourth Value: ${FRUITS[3]}"
