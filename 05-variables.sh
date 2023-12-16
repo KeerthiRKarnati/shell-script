020b8d9d794adb2339ba27d925c9defa0978bc96
Keerthi K <keerthireddyk33@gmail.com>
2023-12-16 18:27:57 +0530

    05-variables.sh

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,4 +5,4 @@ echo "Please enter your username:"
 read USERNAME #the value entered above will be automatically attached to USERNAME variable
 
 
-echo "Username is: $USERNAME"
+# echo "Username is: $USERNAME"
