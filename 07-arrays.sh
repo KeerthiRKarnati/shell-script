be8dfe5b17d08c86621ae2d80d7460a07a6d53ab
Keerthi K <keerthireddyk33@gmail.com>
2023-12-16 18:56:38 +0530

    07-arrays.sh

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -9,3 +9,7 @@ echo "Second Value: ${FRUITS[1]}"
 echo "Third Value: ${FRUITS[2]}"
 
 echo "Fourth Value: ${FRUITS[3]}"
+
+echo "Total number of items:: ${FRUITS[N]}" #no of args 
+
+echo "Printing all items:: ${FRUITS[@]}" #all items
