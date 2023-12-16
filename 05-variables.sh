f62164d9a968dd4677b077536e539b993a9dde65
Keerthi K <keerthireddyk33@gmail.com>
2023-12-16 18:26:21 +0530

    05-variables.sh

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+echo "Please enter your username:"
+
+read USERNAME #the value entered above will be automatically attached to USERNAME variable
+
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
