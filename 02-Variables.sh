a3727c3d64defe39f3809388343907ae53bd8e83
Keerthi K <keerthireddyk33@gmail.com>
2023-12-16 17:42:45 +0530

    02-variables.sh

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Ram
+PERSON2=Raheem
 echo "$PERSON1:: Hello $2PERSON2 , Good Morning"
 echo "$PERSON2:: Hi $PERSON1, Very good morning"
 echo "$PERSON1:: How are you $PERSON2?"
