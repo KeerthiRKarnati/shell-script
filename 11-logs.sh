9851ce91259f08d200e1a363a908c38738bfcbb7
Keerthi K <keerthireddyk33@gmail.com>
2023-12-17 10:53:31 +0530

    11-logs.sh

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,23 +3,27 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "Error:: $2 ... failed"
+        echo "Error:: $2 ... $R FAILED $N"
         exit 1 #if error arises you can't proceed, give other than zero
     else
-        echo "$2 ... success"
+        echo "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "Error:: Please run the script with root user"
+    echo "Error:: Please $R run the script with root user $N"
     exit 1 #if error arises you can't proceed, give other than zero
 else
-    echo "You are root user"
+    echo "You are $G root $N user"
 fi
 
 yum install mysql -y &>> $LOGFILE
