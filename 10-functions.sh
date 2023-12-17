1dc0ad99f33d87639df191d76913459006a9209f
Keerthi K <keerthireddyk33@gmail.com>
2023-12-17 09:50:47 +0530

    10-functions.sh

---
+++ 10-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "Error:: Installing mysql is failed"
+        exit 1 #if error arises you can't proceed, give other than zero
+    else
+        echo "Installing mysql is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "Error:: Please run the script with root user"
+    exit 1 #if error arises you can't proceed, give other than zero
+else
+    echo "You are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
