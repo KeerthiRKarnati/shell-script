73c6337b6d7c1c0602614e0cbf6d65fa78404f30
Keerthi K <keerthireddyk33@gmail.com>
2023-12-17 10:57:42 +0530

    11-logs.sh

--- 11-logs.sh
+++ 11-logs.sh
@@ -10,19 +10,19 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "Error:: $2 ... $R FAILED $N"
+        echo  -e "Error:: $2 ... $R FAILED $N"
         exit 1 #if error arises you can't proceed, give other than zero
     else
-        echo "$2 ... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "Error:: Please $R run the script with root user $N"
+    echo -e "Error:: Please $R run the script with root user $N"
     exit 1 #if error arises you can't proceed, give other than zero
 else
-    echo "You are $G root $N user"
+    echo -e "You are $G root $N user"
 fi
 
 yum install mysql -y &>> $LOGFILE
